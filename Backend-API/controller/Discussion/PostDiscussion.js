ed2d73c491ed3736acddcbb828c1ea53bb79dbf7
Beloved1310 <adeoluwafisayomi@gmail.com>
2023-03-08 01:43:10 -0800

    feat/update discussion

--- Backend-API/controller/Discussion/PostDiscussion.js
+++ Backend-API/controller/Discussion/PostDiscussion.js
@@ -1,15 +1,16 @@
 const Discussion = require('../../model/Discussion')
 const User = require('../../model/User')
 const postDiscusion = require('../../validation/Discussion/postDisscussion')
+const cloudinary = require('../../utilis/cloudinary')
 
 
 module.exports = async (req, res) => {
   const { value, error } = postDiscusion(req.body)
   if (error) return res.status(400).send({ error: error.details[0].message })
   const { title, author, body } = value
-  const user = await User.findOne({ _id: req.user._id }).select(
-    'fullname email -_id',
-  )
+  // const user = await User.findOne({ _id: req.user._id }).select(
+  //   'fullname email -_id',
+  // )
 
   const {
     secure_url: image,
@@ -21,13 +22,13 @@ module.exports = async (req, res) => {
     title,
     author,
     body,
-    user,
+    // user,
   })
   const data = {
     image,
     title,
     author,
-    user,
+    // user,
     body,
   }
   return res.status(200).json({ message: 'Discussion Post created', data })
--- Backend-API/routes/Discussion.js
+++ Backend-API/routes/Discussion.js
@@ -1,13 +1,22 @@
-// const multer = require('multer')
+const multer = require('multer')
 const express = require('express')
 
 const router = express.Router()
+const storage = require('../utilis/multer')
+
+const upload = multer({ storage })
 
 const PostDiscussion = require('../controller/Discussion/PostDiscussion')
 const updateDiscussion = require('../controller/Discussion/updateDiscussion')
 const deleteDiscussion = require('../controller/Discussion/deleteDiscussion')
+const getAllDisscussion = require('../controller/Discussion/getAllDisscussion')
+const getDisscussionId = require('../controller/Discussion/getDisscussionId')
 
-router.post('/', PostDiscussion)
+router.get('/', getAllDisscussion)
+
+router.get('/:id', getDisscussionId)
+
+router.post('/', upload.single('image'), PostDiscussion)
 
 router.put('/:id', updateDiscussion)
 
