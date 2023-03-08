0ca58e6ecb6763bbef5eefbf5e8686769d3782d5
tosmel2 <adewaletosin0808@gmail.com>
2023-03-08 18:34:58 +0100

    I made some modification on these files to work on my machine, not necessary to be merge to main if the main working fine with the user controler and routes

--- Backend-API/index.js
+++ Backend-API/index.js
@@ -1,14 +1,19 @@
-const express = require('express')
-const cors = require('cors')
-require('dotenv').config()
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { dbConnect } from './config/dbConnect.js';
+import courseRoutes from './routes/CourseRoutes.js';
+import cartPurchaseRoutes from './routes/CartPurchaseRoutes.js';
+import userRoutes from './routes/UserRoutes.js';
+// import discussion from './routes/Discussion.js';
+// import comment from './routes/Comment.js';
+// import { authenticateToken } from './middleware/jwt.js';
+dotenv.config();
+dbConnect();
 // const debug = require('debug')('app')
-const PORT = process.env.PORT
 
 const app = express()
-require('./db/db')()
-
-const Discussion = require('./routes/Discussion')
-const Comment = require('./routes/Comment')
+// require('./db/db')()
 
 process.on('unhandledRejection', (err) => {
   console.log(err, 'Unhandled Rejection at Promise')
@@ -24,10 +29,16 @@ app.use(cors({ origin: '*' }))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json({ limit: '50mb' }))
 
-app.use('/discussion', Discussion)
-app.use('/comment', Comment)
+app.use('/api/v1/courses', courseRoutes);
+app.use('/api/v1/cartPurchase', cartPurchaseRoutes);
+app.use('/api/v1/user', userRoutes);
+// app.use('/api/v1/discussions', discussion);
+// app.use('/api/v1/comments', comment);
 
+// app.use('/discussion', Discussion)
+// app.use('/comment', Comment)
+// app.use('/user', UserRoutes)
 
-app.listen(PORT, () => {
-  console.log(`Web server is running ${PORT}`)
-})
+//listen server
+const PORT = process.env.Port || 5000;
+app.listen(PORT, () => console.log(`Web server running on port ${PORT}`)); 
