c7555af447c245a4f785e7818c23894db9327f8c
Beloved1310 <adeoluwafisayomi@gmail.com>
2023-03-15 19:30:09 -0700

    merge conflicts

--- Backend-API/index.js
+++ Backend-API/index.js
@@ -3,6 +3,8 @@ import cors from 'cors'
 
 import * as dotenv from 'dotenv' 
 import { dbConnection } from './db/db';
+import Discussion from './routes/Discussion'
+import Comment from './routes/Comment'
 dotenv.config()
 // const debug = require('debug')('app')
 const PORT = process.env.PORT
@@ -11,8 +13,6 @@ const app = express();
 
 dbConnection();
 
-import Discussion from './routes/Discussion'
-import Comment from './routes/Comment'
 
 process.on('unhandledRejection', (err) => {
   console.log(err, 'Unhandled Rejection at Promise')
@@ -31,8 +31,8 @@ app.use(express.json({ limit: '50mb' }))
 app.use('/api/v1/courses', courseRoutes);
 app.use('/api/v1/cartPurchase', cartPurchaseRoutes);
 app.use('/api/v1/user', userRoutes);
-// app.use('/api/v1/discussions', discussion);
-// app.use('/api/v1/comments', comment);
+app.use('/api/v1/discussions', Discussion);
+app.use('/api/v1/comments', Comment);
 
 // app.use('/discussion', Discussion)
 // app.use('/comment', Comment)
